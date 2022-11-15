2abc7f15e212a56cef2b2b1a7ed02c6e0fd14af0
Jara <jaraguta03@gmail.com>
2022-11-15 20:33:18 +0300

    add html file to grabbmessage from

--- email_sender.py
+++ email_sender.py
@@ -11,6 +11,7 @@ email.set_content('visit our shop to collect ur gift card')
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
     smtp.starttls()
-    smtp.login('testdummytrue@gail.com', 'dummypassword')
+    # since google stopped less secure app acess u need to allow 2FA and add app password
+    smtp.login('testdummytrue@gmail.com', 'dummypassword')
     smtp.send_message(email)
     print('task done!!!')
